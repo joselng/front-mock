e877275e5f9e3ed038572fcf90687d3c0bb54634
GFT\anaf <alan.franca@gft.com>
2022-12-07 15:44:46 -0300

    chore: added new api client

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,4 +5,9 @@ const api = axios.create({
     baseURL: `https://65pwcy7ng5.execute-api.us-east-1.amazonaws.com/v1/open-insurance/consents/v1/`,
 })
 
+export const apiBFF = axios.create({
+    // baseURL: `https://api-openinsurance.mapfre.com.br/open-insurance/consents/v1/`,
+    baseURL: `https://65pwcy7ng5.execute-api.us-east-1.amazonaws.com/v1/bff/consent/`,
+})
+
 export default api
